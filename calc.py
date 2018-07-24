f5f31a86915109382a993a840dfd14abda2f45df
nahome getachew <nahomg@gmail.com>
2018-07-24 10:31:49 -0500

    initial commit

---
+++ calc.py
@@ -0,0 +1,13 @@
+class Arthemetic():
+
+    def add(self, x, y):
+        pass
+
+    def subtract(self, x, y):
+        pass
+
+    def multiply(self, x, y):
+        pass
+
+    def divide(self, x, y):
+        pass
